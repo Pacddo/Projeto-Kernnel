ed65cc5452770b01858a0f84d053c3377952519b
pacddo <douglas.pacheco@hotmail.com>
2025-02-17 13:04:02 -0300

    first commit

---
+++ Assistente/Program.cs
@@ -0,0 +1,35 @@
+﻿using Azure.AI.OpenAI; 
+using Azure;
+using OpenAI.Chat;
+
+string openAIEndpoint = "";
+string openAIAPIKey = "";
+string openAIDeploymentName = "";
+
+var endpoint = new Uri(openAIEndpoint);
+var credentials = new AzureKeyCredential(openAIAPIKey);
+
+var openAIClient = new AzureOpenAIClient(endpoint, credentials);
+
+var systemPrompt = """
+You are a game enthusiast who helps people discover best games. You are nerd and friendly. 
+You ask people what type of games they like to take and then suggest some.
+""";
+#pragma warning restore CS0219 // Variable is assigned but its value is never used
+
+List<ChatMessage> chatHistory = new();
+SystemChatMessage systemMessage = ChatMessage.CreateSystemMessage(systemPrompt);
+
+chatHistory.Add(systemMessage);
+
+string userGreeting =
+"Olá, me recomende um jogo para PS4";
+
+UserChatMessage userGreetingMessage = ChatMessage.CreateUserMessage(userGreeting);
+chatHistory.Add(userGreetingMessage);
+
+Console.WriteLine($"User >>> {userGreeting}");
+
+var chatClient = openAIClient.GetChatClient(openAIDeploymentName);
+var response = await chatClient.CompleteChatAsync(chatHistory);
+Console.WriteLine($"AI >>> {response.Value.Content.Last().Text}");
---
+++ LightAssist/Program.cs
@@ -0,0 +1,102 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using System.ComponentModel;
+using System.Text.Json.Serialization;
+using Microsoft.SemanticKernel;
+
+var modelId = "";
+var endpoint = "";
+var apiKey = "";
+
+var builder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
+
+builder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+
+Kernel kernel = builder.Build();
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+
+kernel.Plugins.AddFromType<LightsPlugin>("Lights");
+
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new() 
+{
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+};
+
+
+var history = new ChatHistory();
+
+
+string? userInput;
+do {
+    
+    Console.Write("User > ");
+    userInput = Console.ReadLine();
+
+    
+    history.AddUserMessage(userInput);
+
+   
+    var result = await chatCompletionService.GetChatMessageContentAsync(
+        history,
+        executionSettings: openAIPromptExecutionSettings,
+        kernel: kernel);
+
+    
+    Console.WriteLine("Assistant > " + result);
+
+    
+    history.AddMessage(result.Role, result.Content ?? string.Empty);
+} while (userInput is not null);
+
+public class LightsPlugin
+{
+   
+   private readonly List<LightModel> lights = new()
+   {
+      new LightModel { Id = 1, Name = "Table Lamp", IsOn = false },
+      new LightModel { Id = 2, Name = "Porch light", IsOn = false },
+      new LightModel { Id = 3, Name = "Chandelier", IsOn = true }
+   };
+
+   [KernelFunction("get_lights")]
+   [Description("Gets a list of lights and their current state")]
+   public async Task<List<LightModel>> GetLightsAsync()
+   {
+      return lights;
+   }
+
+   [KernelFunction("change_state")]
+   [Description("Changes the state of the light")]
+   public async Task<LightModel?> ChangeStateAsync(int id, bool isOn)
+   {
+      var light = lights.FirstOrDefault(light => light.Id == id);
+
+      if (light == null)
+      {
+         return null;
+      }
+
+ 
+      light.IsOn = isOn;
+
+      return light;
+   }
+}
+
+public class LightModel
+{
+   [JsonPropertyName("id")]
+   public int Id { get; set; }
+
+   [JsonPropertyName("name")]
+   public string Name { get; set; }
+
+   [JsonPropertyName("is_on")]
+   public bool? IsOn { get; set; }
+}
